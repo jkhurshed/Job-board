d7011a616a08e30352b9b77f2cd1dce30c83b344
jkhurshed <jkhurshed002@gmail.com>
2023-06-22 10:56:20 +0500

    commit tests

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""default: {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+    }"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
