d52ea6fd4af6965056472b1cb02aff0ae565a9c7
jkhurshed <jkhurshed002@gmail.com>
2023-06-29 09:18:22 +0500

    commit tests

--- common/models/base.py
+++ common/models/base.py
@@ -2,9 +2,6 @@ from .title_description import TitleDescriptionModel
 
 
 class BaseModel(TitleDescriptionModel):
-    """
-    Abstact Base model with uuid pk and create and update time
-    """
 
     class Meta:
         abstract = True
---
+++ jobs/models/job.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+from uuid import uuid4
+
+from common.models import BaseModel
+
+from contents.models import Location
+
+from employers.models import Company
+
+
+class Job(BaseModel, models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, default=uuid4)
+    # company_id = models.UUIDField(default=uuid4)
+    salary = models.DecimalField("Salary", max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
