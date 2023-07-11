25cf32e2fb500a464158b5cb7bbac074e24ef851
jkhurshed <jkhurshed002@gmail.com>
2023-07-11 14:06:22 +0500

    job model modifications

--- jobs/models/job.py
+++ jobs/models/job.py
@@ -15,5 +15,5 @@ class Job(BaseModel, models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    salary = models.DecimalField("Salary", max_digits=5, decimal_places=2)
+    salary = models.DecimalField("Salary", max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
